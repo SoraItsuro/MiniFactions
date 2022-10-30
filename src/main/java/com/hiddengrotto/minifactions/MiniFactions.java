c69a73898764a90ea4a46949d47343c7ef959fe8
Sora Itsuro <mitch817@gmail.com>
2022-10-30 03:08:08 -0400

    Initial commit

---
+++ src/main/java/com/hiddengrotto/minifactions/MiniFactions.java
@@ -0,0 +1,17 @@
+package com.hiddengrotto.minifactions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MiniFactions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
